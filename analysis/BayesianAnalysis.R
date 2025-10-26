86cdf5fd0e29cd26e9975d9a2c7b647a1b8725ee
nielswd23 <nielsd@uci.edu>
2025-10-26 10:19:02 -0700

    PUDDLE incremental v2 batch

--- analysis/BayesianAnalysis.R
+++ analysis/BayesianAnalysis.R
@@ -22,8 +22,8 @@ fit_and_predict_bayesian <- function(train_dataset, seed, gram, model){
   # if there is more than one value for RS
   if (length(unique(train_dataset$RS)) > 1){
     print("more than one RS value")
-    # my_bf <- bf(List ~ scale(pos_uni_score_smoothed)*scale(pos_bi_score_smoothed)+(1|RS))
-    my_bf <- bf(List ~ scale(uni_prob)*scale(bi_prob_smoothed)+(1|RS))
+    my_bf <- bf(List ~ scale(pos_uni_score_smoothed)*scale(pos_bi_score_smoothed)+(1|RS))
+    # my_bf <- bf(List ~ scale(uni_prob)*scale(bi_prob_smoothed)+(1|RS))
     my_priors <- c(
       prior(normal(0, 1), class = "b"),
       prior(normal(0, 2.5), class = "Intercept"),
@@ -31,14 +31,14 @@ fit_and_predict_bayesian <- function(train_dataset, seed, gram, model){
     )
   } else{
     print("only one RS value")
-    # my_bf <- bf(List ~ scale(pos_uni_score_smoothed)*scale(pos_bi_score_smoothed))
-    my_bf <- bf(List ~ scale(uni_prob)*scale(bi_prob_smoothed))
+    my_bf <- bf(List ~ scale(pos_uni_score_smoothed)*scale(pos_bi_score_smoothed))
+    # my_bf <- bf(List ~ scale(uni_prob)*scale(bi_prob_smoothed))
     my_priors <- c(
       prior(normal(0, 1), class = "b"),
       prior(normal(0, 2.5), class = "Intercept")
     )
   }
-  dir.create("./KFoldModelFits_ngram_incremental/PUDDLE")
+  dir.create("./KFoldModelFits_positional_incremental_v2/PUDDLE")
   print("fitting main model")
   b_model <- brm(my_bf,
                  data = train_dataset,
@@ -50,8 +50,8 @@ fit_and_predict_bayesian <- function(train_dataset, seed, gram, model){
                  prior = my_priors,
                  thin = 1,
                  seed = seed,
-                 save_model = paste0("./KFoldModelFits_ngram_incremental/PUDDLE/",gram,"_prob_",model,"_brmsSeed_",seed,".stan"),
-                 file = paste0("./KFoldModelFits_ngram_incremental/PUDDLE/",gram,"_prob_",model,"_brmsSeed_",seed,"fit"),
+                 save_model = paste0("./KFoldModelFits_positional_incremental_v2/PUDDLE/",gram,"_prob_",model,"_brmsSeed_",seed,".stan"),
+                 file = paste0("./KFoldModelFits_positional_incremental_v2/PUDDLE/",gram,"_prob_",model,"_brmsSeed_",seed,"fit"),
                  # save_model = paste0("./KFoldModelFits_positional/",gram,"_prob_",model,"_brmsSeed_",seed,".stan"),
                  # file = paste0("./KFoldModelFits_positional/",gram,"_prob_",model,"_brmsSeed_",seed,"fit"),
                  # save_model = paste0("./KFoldModelFits_ngram/",gram,"_prob_",model,"_brmsSeed_",seed,".stan"),
@@ -201,7 +201,7 @@ read_in_candidate_lexicons <- function(lexicon_file_name){
 
 # get the list of files to iterate through
 # all_scored_lists <- list.files("../ScoredLists/standard", recursive = TRUE, full.names = TRUE)
-all_scored_lists <- list.files("../ScoredLists/incremental/PUDDLE", recursive = TRUE, full.names = TRUE)
+all_scored_lists <- list.files("../ScoredLists/incremental_v2/PUDDLE", recursive = TRUE, full.names = TRUE)
 all_scored_basenames <- basename(all_scored_lists)
 
 # global lists (just the filenames)
@@ -271,7 +271,7 @@ check_list_compliance(all_scored_lists)
 
 # get a list of the model names (folder names)
 # list_of_model_types <- list.dirs("../ScoredLists/standard", recursive = FALSE, full.names = FALSE)
-list_of_model_types <- list.dirs("../ScoredLists/incremental/PUDDLE", recursive = FALSE, full.names = FALSE)
+list_of_model_types <- list.dirs("../ScoredLists/incremental_v2/PUDDLE", recursive = FALSE, full.names = FALSE)
 # priority_list = c("PearlBrentUtterances", "PearlBrentWords", "TinyInfantLexiconNoNumbers_Prepped")
 # priority_list = c("OLDPearlCorpusUtterances", "OLDPearlCorpusWordTypes")
 priority_list = c("OLDTinyInfantLexiconNoNumbers_Prepped")
@@ -331,7 +331,7 @@ for (model in list_of_model_types[c(7,8)]) {
   print(res)
   
   # write_csv(res, paste0("./Results_positional/", model, "_auto_kfold.csv"))
-  dir.create("./Results_incremental_ngram/PUDDLE")
-  write_csv(res, paste0("./Results_incremental_ngram/PUDDLE/", model, "_auto_kfold.csv"))
+  dir.create("./Results_incremental_positional_v2/PUDDLE")
+  write_csv(res, paste0("./Results_incremental_positional_v2/PUDDLE/", model, "_auto_kfold.csv"))
 }
 
