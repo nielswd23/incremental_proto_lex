b2bd2ec194f4b9dfc0d5ff814546a39b4e719711
nielswd23 <nielsd@uci.edu>
2026-01-09 11:54:10 -0800

    gold ids incremental batch 1

--- analysis/BayesianAnalysis.R
+++ analysis/BayesianAnalysis.R
@@ -38,7 +38,7 @@ fit_and_predict_bayesian <- function(train_dataset, seed, gram, model){
       prior(normal(0, 2.5), class = "Intercept")
     )
   }
-  # dir.create("./KFoldModelFits_positional_incremental_v2/AGSimple")
+  dir.create("./KFoldModelFits_positional_incremental_v2/PearlBrentWords") # for incremental runs only
   print("fitting main model")
   b_model <- brm(my_bf,
                  data = train_dataset,
@@ -200,8 +200,8 @@ read_in_candidate_lexicons <- function(lexicon_file_name){
 ## END readin function ##
 
 # get the list of files to iterate through
-all_scored_lists <- list.files("../ScoredLists/standard", recursive = TRUE, full.names = TRUE)
-# all_scored_lists <- list.files("../ScoredLists/incremental_v2/AGSimple", recursive = TRUE, full.names = TRUE)
+# all_scored_lists <- list.files("../ScoredLists/standard", recursive = TRUE, full.names = TRUE)
+all_scored_lists <- list.files("../ScoredLists/incremental_v2/PearlBrentWords", recursive = TRUE, full.names = TRUE)
 all_scored_basenames <- basename(all_scored_lists)
 
 # global lists (just the filenames)
@@ -270,15 +270,15 @@ check_list_compliance(all_scored_lists)
 
 
 # get a list of the model names (folder names)
-list_of_model_types <- list.dirs("../ScoredLists/standard", recursive = FALSE, full.names = FALSE)
-# list_of_model_types <- list.dirs("../ScoredLists/incremental_v2/AGSimple", recursive = FALSE, full.names = FALSE)
+# list_of_model_types <- list.dirs("../ScoredLists/standard", recursive = FALSE, full.names = FALSE)
+list_of_model_types <- list.dirs("../ScoredLists/incremental_v2/PearlBrentWords", recursive = FALSE, full.names = FALSE) # for incremental runs
 # priority_list = c("PearlBrentUtterances", "PearlBrentWords", "TinyInfantLexiconNoNumbers_Prepped")
 # priority_list = c("OLDPearlCorpusUtterances", "OLDPearlCorpusWordTypes")
-priority_list = c("top_22ContentCleaned")
+# priority_list = c("top_22ContentCleaned")
 
 ### Running model ###
-for (model in priority_list) {
-# for (model in list_of_model_types[c(13:15)]) {
+# for (model in priority_list) { # for listing segmentation type to run
+for (model in list_of_model_types[c(1:6)]) {
   set.seed(seed)
   print(paste0("working on current model ", model))
   
@@ -331,8 +331,8 @@ for (model in priority_list) {
   print(res)
   
   # write_csv(res, paste0("./Results_positional/", model, "_auto_kfold.csv"))
-  write_csv(res, paste0("./Results_ngram/", model, "_auto_kfold.csv"))
-  # dir.create("./Results_incremental_positional_v2/AGSimple")
-  # write_csv(res, paste0("./Results_incremental_positional_v2/AGSimple/", model, "_auto_kfold.csv"))
+  # write_csv(res, paste0("./Results_ngram/", model, "_auto_kfold.csv"))
+  dir.create("./Results_incremental_positional_v2/PearlBrentWords") # for incremental we need to create dir first because we have a folder for each segmentation type and then results for each slice within
+  write_csv(res, paste0("./Results_incremental_positional_v2/PearlBrentWords/", model, "_auto_kfold.csv"))
 }
 
