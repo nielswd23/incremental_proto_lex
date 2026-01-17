76d261e298bcc0640acfb0311a919a31fc90d083
nielswd23 <nielsd@uci.edu>
2026-01-17 13:11:30 -0800

    updated counts

--- splitting_corpora.py
+++ splitting_corpora.py
@@ -136,9 +136,9 @@ output_root = "./incremental_corpora_out_v2"
 # levels = [512, 128, 64, 32, 16, 8, 4, 2]
 # samples_per_level = {512: 8, 128: 8, 64: 8, 32: 8, 16: 8, 8: 8, 4: 4, 2: 2}
 # levels = [64, 32, 16, 8, 4, 2, 1.75, 1.5, 1.25, 1.1, 1.05, 1.025, 1.01]
-levels = [1.02, 1.015]
+levels = [1.0175]
 # samples_per_level = {64: 8, 32: 8, 16: 8, 8: 8, 4: 8, 2: 8, 1.75: 8, 1.5:8, 1.25:8, 1.1:8, 1.05:8, 1.025:8, 1.01:8}
-samples_per_level = {1.02:8, 1.015:8}
+samples_per_level = {1.0175:8}
 random_seed = 42
 
 # Explicit list of folders you want to process
